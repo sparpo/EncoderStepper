3354844e1933db898985cfad0629de10f9913f17
Joey Corbett <joeyecorbett@gmail.com>
2022-10-20 10:23:30 +0100

    Volatile variables, rolling average (not tested)

--- encoder_reader/encoder_reader.ino
+++ encoder_reader/encoder_reader.ino
@@ -3,6 +3,7 @@
 #include <Adafruit_SSD1306.h>
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define avgSize 10
 
 // declare an SSD1306 display object connected to I2C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
@@ -17,12 +18,17 @@ int directionPin = 4;
 int dir = 1;
 float outputDecimal = 0;
 int outputGray = 0;
-volatile int counter = 0;
-long currentTime;
-long previousTime;
-long timeDif;
-
 long counterFreq;
+
+//rolling average 
+float rollingRPM[avgSize] = {0};
+float avgRPM;
+
+//all variables used in interrupt must be declared volatile
+volatile int counter = 0;
+volatile long currentTime;
+volatile long previousTime;
+volatile long timeDif;
 
 void setup() {
   Serial.begin(9600);
@@ -36,14 +42,14 @@ void setup() {
   pinMode(incrementalPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(incrementalPin), count, RISING);
 
-  //
+  //set up output pins
   pinMode(stepPin, OUTPUT);
   pinMode(directionPin, OUTPUT);
   analogWrite(stepPin, 127);
   digitalWrite(directionPin, dir);
   
   
-  currentTime = micros();
+  currentTime = millis();
   //setup OLED
   // initialize OLED display with address 0x3C for 128x64
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -74,10 +80,10 @@ void loop() {
   outputDecimal = inversegrayCode(outputGray)* 11.25; //5bits = 32 segments. 360/32 = 11.25 degrees
   
   //incremental counter
-  if (counter > 70) {
+  if (counter >= 70) {
     dir = 0;
   }
-  if (counter < 10) {
+  if (counter <= 10) {
     dir = 1;
   }
   String angle = String(outputDecimal) + "°";
@@ -92,13 +98,29 @@ void loop() {
   String timing = String(counter) + "," + timeDif;
   
   //if we know freqnecy of counters
+  counterFreq = 1/timeDif;
   //One counter corresponds to 6 degrees
   //Angles per second = 6 * counters * counterfreq => 6*1*freq
-  long omega = 6 * counterFreq; //   (angles per s)/360 = cycles per second
+  long omega = 6 * counterFreq; 
+  //(angles per s)/360 = cycles per second, frequency
   long freq = omega/360;
   // 1hz = 60 rpm
   float rpm = freq * 60;
-  Serial.println(counter);
+
+  //fill out rolling average array
+  for(i=1; i < avgSize; i++) {
+    rollingRPM[i] = rollingRPM[i - 1]; //move array items back one
+    avgRPM += rollingRPM[i];
+  }
+  rollingRPM[0] = rpm; //insert newest value into array
+
+  //find average
+  for(i=0; i < avgSize; i++) {
+    avgRPM += rollingRPM[i]; //get total
+  }
+  avgRPM = avgRPM/avgSize; //divide by array size
+  
+  Serial.println(counter, avgRPM);
   
 }
 
@@ -114,7 +136,7 @@ void count() {
   previousTime = currentTime;
   currentTime = millis();
   timeDif = currentTime - previousTime;
-  counterFreq = 1/timeDif;
+  
   if (dir == 1) {
     counter++;
   } else {
