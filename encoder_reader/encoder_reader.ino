5d1567f07340fffe8839b3db61074b5ad4e9f667
Joey Corbett <joeyecorbett@gmail.com>
2022-10-20 18:30:39 +0100

    fixed bugs, rpm measurement not working

--- encoder_reader/encoder_reader.ino
+++ encoder_reader/encoder_reader.ino
@@ -8,6 +8,7 @@
 // declare an SSD1306 display object connected to I2C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+int wave = 0;
 //pins
 int absolutePins[5] = {9,10,11,12,13};
 int incrementalPin = 8;
@@ -16,9 +17,9 @@ int directionPin = 4;
 
 //global variables
 int dir = 1;
-float outputDecimal = 0;
+
 int outputGray = 0;
-long counterFreq;
+long counterFreq = 0;
 
 //rolling average 
 float rollingRPM[avgSize] = {0};
@@ -26,15 +27,17 @@ float avgRPM;
 
 //all variables used in interrupt must be declared volatile
 volatile int counter = 0;
-volatile long currentTime;
-volatile long previousTime;
-volatile long timeDif;
+volatile long currentTime = 0;
+volatile long previousTime = 0;
+volatile long timeDif = 0;
+
+int prevCounter = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   // put your setup code here, to run once:
   int i = 0;
-  for(i=0;i<6;i++) {
+  for(i=0;i<5;i++) {
     pinMode(absolutePins[i], INPUT_PULLUP);
   }
 
@@ -45,68 +48,70 @@ void setup() {
   //set up output pins
   pinMode(stepPin, OUTPUT);
   pinMode(directionPin, OUTPUT);
+   TCCR0B = 0b00000010; // x8
+  TCCR0A = 0b00000001; // phase correct
   analogWrite(stepPin, 127);
   digitalWrite(directionPin, dir);
-  
+
   
   currentTime = millis();
-  //setup OLED
-  // initialize OLED display with address 0x3C for 128x64
-  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    while (true);
-  }
-  delay(1000);         // wait for initializing
-  oled.clearDisplay(); // clear display
-  oled.setTextColor(WHITE);     // text color
-  oled.setTextSize(1);          // text size
-  oled.setCursor(0, 10);        // position to display
-  oled.println("Hello World!"); // text to display
-  oled.display();               // show on OLED
-  
+
+  setupOLED();
   
 }
 
 void loop() {
+  //Serial.println(counter, avgRPM);
   int i = 0;
   //String out = "";
   digitalWrite(directionPin, dir);
-  
+  outputGray = 0;
   //first 5 bits (A,B,C,D,E) are absolute encoder
   for(i=0;i<5;i++) {
     int val = digitalRead(absolutePins[i]);
     outputGray = ((outputGray<<1) | val); //combine digital readings into one gray code
   }
-  outputDecimal = inversegrayCode(outputGray)* 11.25; //5bits = 32 segments. 360/32 = 11.25 degrees
+  float outputDecimal = inversegrayCode(outputGray)* 11.25; //5bits = 32 segments. 360/32 = 11.25 degrees
   
   //incremental counter
-  if (counter >= 70) {
+  if (counter >= 60) {
     dir = 0;
   }
   if (counter <= 10) {
     dir = 1;
   }
-  String angle = String(outputDecimal) + "°";
   
-  oled.clearDisplay(); // clear display
-  oled.setCursor(0, 10);        // position to display
-  oled.println(angle); // text to display
-  oled.setCursor(0, 20);        // position to display
-  oled.println(counter); // text to display
-  oled.display();               // show on OLED
+  if(digitalRead(incrementalPin) == 1 && wave == 0) {
+    wave = 1;
+    if (dir == 1) {
+      counter++;
+    } else {
+      counter--;
+    }
+    
+    /*previousTime = currentTime;
+    
+    timeDif = currentTime - previousTime;
+    currentTime = millis();*/
+  }
+  if(digitalRead(incrementalPin) == 0 && wave == 1) {
+    
+   wave = 0;
+  }
+
 
   String timing = String(counter) + "," + timeDif;
   
   //if we know freqnecy of counters
-  counterFreq = 1/timeDif;
+  counterFreq = 1.0/timeDif;
   //One counter corresponds to 6 degrees
   //Angles per second = 6 * counters * counterfreq => 6*1*freq
-  long omega = 6 * counterFreq; 
+  long omega = 6.0 * counterFreq; 
   //(angles per s)/360 = cycles per second, frequency
-  long freq = omega/360;
+  long freq = omega/360.0;
   // 1hz = 60 rpm
-  float rpm = freq * 60;
-
+  float rpm = freq * 60.0;
+/*
   //fill out rolling average array
   for(i=1; i < avgSize; i++) {
     rollingRPM[i] = rollingRPM[i - 1]; //move array items back one
@@ -119,9 +124,12 @@ void loop() {
     avgRPM += rollingRPM[i]; //get total
   }
   avgRPM = avgRPM/avgSize; //divide by array size
-  
-  Serial.println(counter, avgRPM);
-  
+  */
+  //Serial.println(counter);
+  //Serial.println(rpm);
+  Serial.println(counter);
+  Serial.println(rpm);
+  updateOLED(outputDecimal, counter, rpm);
 }
 
 int inversegrayCode(int n) {
@@ -133,13 +141,45 @@ int inversegrayCode(int n) {
     return inv;
 }
 void count() {
-  previousTime = currentTime;
-  currentTime = millis();
-  timeDif = currentTime - previousTime;
+  Serial.println("hello");
+  //counter++;
+  //previousTime = currentTime;
+  //currentTime = millis();
+  //timeDif = currentTime - previousTime;
   
-  if (dir == 1) {
-    counter++;
-  } else {
-    counter--;
+  //if (dir == 1) {
+    
+  //} else {
+  //  counter--;
+  //}
+}
+
+void updateOLED(float angle, int counter, float rpm){
+  oled.setTextSize(2);
+  oled.clearDisplay(); // clear display
+  oled.setCursor(0, 0);
+  oled.println(String(angle) + " deg"); // text to display
+  oled.setCursor(20, 30);        // position to display
+  oled.println(String(0) + " rpm"); // text to display
+  
+  oled.setTextSize(1);
+  oled.setCursor(0, 50);        // position to display
+  oled.println(String(counter) + " counts"); // text to display
+
+  oled.display();               // show on OLED
+}
+void setupOLED() {
+  //setup OLED
+  // initialize OLED display with address 0x3C for 128x64
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
   }
+  delay(1000);         // wait for initializing
+  oled.clearDisplay(); // clear display
+  oled.setTextColor(WHITE);     // text color
+  oled.setTextSize(2);          // text size
+  oled.setCursor(0, 10);        // position to display
+  //oled.println("Hello World!"); // text to display
+  oled.display();               // show on OLED
 }
